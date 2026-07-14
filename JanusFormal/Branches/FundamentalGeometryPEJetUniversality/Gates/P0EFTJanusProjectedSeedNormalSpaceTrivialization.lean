f7da1b6e80982cacb117788511ea644dab789400
trigunino <49878387+trigunino@users.noreply.github.com>
2026-07-15 00:33:47 +0200

    Use direct continuous-linear projection from normal trivialization

--- JanusFormal/Branches/FundamentalGeometryPEJetUniversality/Gates/P0EFTJanusProjectedSeedNormalSpaceTrivialization.lean
+++ JanusFormal/Branches/FundamentalGeometryPEJetUniversality/Gates/P0EFTJanusProjectedSeedNormalSpaceTrivialization.lean
@@ -237,9 +237,8 @@ def ConnectionCorrectedActualJanusLocalJetData.toProjectedSeedActualJanusLocalJe
       (Tangent := Tangent) (Normal := Normal) where
   tangentialQuadratic := data.sourceConnection
   normalQuadratic := transportContinuousSecondFundamentalForm
-    ((projectedSeedNormalTrivialization tangentBasis hTangentBasis
-      normalBasis hNormalBasis basisData hDimension center base hValid)
-        .toContinuousLinearEquiv).toContinuousLinearMap
+    (projectedSeedNormalTrivialization tangentBasis hTangentBasis
+      normalBasis hNormalBasis basisData hDimension center base hValid).toContinuousLinearEquiv.toContinuousLinearMap
     data.normalQuadratic
   connectionValue := data.connectionValue
   connectionDerivative := data.connectionDerivative
