88087c1ce2e5e9f87b9f7b1286fa0eb878907ab5
delifred <alzieu.frederic@gmail.com>
2026-07-16 18:33:49 +0200

    Identify corrected Spin lifts with composition

--- scripts/audit_janus_d8_topology.py
+++ scripts/audit_janus_d8_topology.py
@@ -479,6 +479,7 @@ AMBIENT_SPIN_ATLAS_OBSTRUCTION_DECLARATIONS = (
     "theorem ambientSpinCechDefect_third_kernelTranslate",
     "theorem ambientSpinCechDefect_canonicalThirdCorrection_eq_one",
     "theorem ambientSpinCechDefect_third_kernelTranslate_eq_one_iff",
+    "theorem ambientSpinCechCanonicalThirdCorrection_eq_comp",
     "theorem ambientSpinCechDefect_secondThird_kernelTranslate",
     "theorem ambientSpinCechDefect_firstSecond_kernelTranslate",
 )
